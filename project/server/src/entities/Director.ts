31f7b989063b570a278497624b5f2d96f10cc2ec
kaksi018 <jeongyu70@gmail.com>
2024-09-06 09:22:03 +0900

    4.1.7_페이지네이션 오류수정

--- project/server/src/entities/Director.ts
+++ project/server/src/entities/Director.ts
@@ -2,11 +2,6 @@ import { Field, Int, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class Director {
-<<<<<<< HEAD
   @Field(() => Int,{description:'감독아이디'}) id: number;
   @Field(() => String,{description:'감독이름'}) name: string;
-=======
-  @Field(() => Int) id: number;
-  @Field(() => String) name: string;
->>>>>>> ec80aa9b862e302133eb2e8a6c336d1c9f3adc73
 }
